351d740483390c5adf2df16c75e4c9af34c67919
ali203939 <ali.brito@sempreceub.com>
2025-03-12 20:22:43 -0300

    Add files via upload

---
+++ mediaNotas.py
@@ -0,0 +1,12 @@
+nota1 = float(input("Nota1: "))
+nota2 = float(input("Nota2: "))
+
+media = (nota1 + nota2)/2
+
+print("Media = ", media)
+
+if media >= 5:
+    print("Aprovado")
+
+else:
+    print("Reprovado")
