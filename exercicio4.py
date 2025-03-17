63bb6870d00463283e850bef4c6a936d0320930d
ali203939 <ali.brito@sempreceub.com>
2025-03-17 18:51:13 -0300

    Rename exercicio4.py to numeroMaior.py

--- exercicio4.py
+++
@@ -1,7 +0,0 @@
-numeroUm = int(input("Primeiro número: "))
-numeroDois = int(input("Segundo número: "))
-
-if numeroUm > numeroDois:
-    print("Ordem crescente: ", numeroUm, numeroDois)
-else:
-    print("Ordem crescente: ", numeroDois,numeroUm)
---
+++ numeroMaior.py
@@ -0,0 +1,7 @@
+numeroUm = int(input("Primeiro número: "))
+numeroDois = int(input("Segundo número: "))
+
+if numeroUm > numeroDois:
+    print("Ordem crescente: ", numeroUm, numeroDois)
+else:
+    print("Ordem crescente: ", numeroDois,numeroUm)
